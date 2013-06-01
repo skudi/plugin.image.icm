d8fc4de0b48c1507b862120a0e7b5158f4aac7a3
Michał Skuza <michalskuza76@gazeta.pl>
2013-06-01 22:43:35 +0200

    borsk, windfimder

--- addon.py
+++ addon.py
@@ -6,6 +6,7 @@ import xbmcplugin
 import os
 import xbmcaddon
 import sys
+import datetime
 
 def addImageLink(name,url):
     addon = xbmcaddon.Addon(id='plugin.image.icm')
@@ -15,9 +16,16 @@ def addImageLink(name,url):
     ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,isFolder=False)
     return ok
 
-addImageLink('Elbląg', 'http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=352&col=222&lang=pl')
-addImageLink('Gdańsk', 'http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=346&col=210&lang=pl')
+addImageLink('Elbląg',   'http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=352&col=222&lang=pl')
+addImageLink('Gdańsk',   'http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=346&col=210&lang=pl')
 addImageLink('Chłapowo', 'http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=334&col=206&lang=pl')
+addImageLink('Borsk',    'http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=360&col=199&lang=pl')
+chour = datetime.datetime.now().hour
+tzshift=2 #windifnder does'n respect summer time, always CET?
+for xhour in range(chour, chour+18):
+    hour = xhour % 24
+    if (hour > 20 and hour < 9): continue
+    addImageLink('windfinder %02d' % (hour), 'http://www.windfinder.com/grafiken/forecasts/superforecast_poland_baltic_sea%02d.png' % (xhour - tzshift))
 
 print sys.builtin_module_names
 
